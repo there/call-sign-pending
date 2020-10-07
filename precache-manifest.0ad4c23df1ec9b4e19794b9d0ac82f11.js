26cce43a815f1b325d65e81a81b0c7c53337320e
Theresa Phan <there@users.noreply.github.com>
2020-10-06 23:58:09 -0400

    Updates

---
+++ precache-manifest.0ad4c23df1ec9b4e19794b9d0ac82f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f983ac9cf9669ea49316e0cff930f42f",
+    "url": "/call-sign-pending/index.html"
+  },
+  {
+    "revision": "0c6b7af5bb16d6c259ed",
+    "url": "/call-sign-pending/static/css/main.cca52357.chunk.css"
+  },
+  {
+    "revision": "d6c923c91478d956810c",
+    "url": "/call-sign-pending/static/js/2.c0410d82.chunk.js"
+  },
+  {
+    "revision": "36157446e0c4e482447c93f6e4f7f144",
+    "url": "/call-sign-pending/static/js/2.c0410d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6b7af5bb16d6c259ed",
+    "url": "/call-sign-pending/static/js/main.bc8e2371.chunk.js"
+  },
+  {
+    "revision": "d9e0636cfef7f3531720",
+    "url": "/call-sign-pending/static/js/runtime-main.149c3f7c.js"
+  }
+]);
--- src/state/index.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
-import inkGame, { INITIAL_STATE } from "./reducers";
-export default createStore(inkGame, INITIAL_STATE, devToolsEnhancer());
